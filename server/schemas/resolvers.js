644e4b3cdd47b9c8776d1a0a668aee8e4f4697a7
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-12-11 15:13:56 -0700

    updated server/auth.js to work with graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,12 +6,11 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v, -password"
-        );
+        const userData = await User.findOne({ _id: context.user._id })
+        .select("-__v, -password");
         return userData;
       }
-      throw new AuthenticationError("You are not logged in!");
+      throw new AuthenticationError("Not logged in!");
     },
   },
 
