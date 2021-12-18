694fb45c52fca5b9eb0eb9766ead64a299b513e2
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-12-17 20:45:44 -0700

    working through npm issues

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import { removeBookId, saveBookIds } from '../utils/localStorage';
+import { removeBookId } from '../utils/localStorage';
 import { GET_ME } from  '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,26 +3,8 @@ const { gql } = require('apollo-server-express');
 
 // create the typeDefs
 const typeDefs = gql`
-type Book {
-    authors: [String]
-    description: String
-    bookId: String!
-    image: String
-    link: String
-    title: String!
-}
-
-type User {
-    _id: ID
-    username: String!
-    email: String!
-    bookCount: Int
-    savedBooks: [Book]
-}
-
-type Auth {
-    token: ID!
-    user: User
+type Query {
+    me: User
 }
 
 input SavedBookInput {
@@ -34,15 +16,33 @@ input SavedBookInput {
     link: String
 }
 
-type Query {
-    me: User
-}
-
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: SavedBookInput): User
     removeBook(bookId: String!): User
+}
+
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
 }
 `;
 
